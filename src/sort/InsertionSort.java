32523b89e28930a019ba1f6cff4e2b89e121391c
vinhbgdm <ninhdinhvinh11012001@gmail.com>
2025-08-04 14:59:57 +0700

    Insertion Sort

---
+++ src/sort/InsertionSort.java
@@ -0,0 +1,22 @@
+package sort;
+
+public class InsertionSort {
+    public static void insertionSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int k = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] > k) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = k;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        insertionSort(arr);
+        System.out.println("Sorted Array");
+    }
+}
