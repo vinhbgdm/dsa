45f507c84de0a2f1e284de52ee0048bb2047aae3
vinhbgdm <ninhdinhvinh11012001@gmail.com>
2025-08-12 21:15:52 +0700

    933. Number of Recent Calls

--- src/stackQueue/StackQueueJava.java
+++ src/stackQueue/StackQueueJava.java
@@ -1,5 +1,7 @@
 package stackQueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class StackQueueJava {
@@ -21,5 +23,22 @@ public class StackQueueJava {
         System.out.println("Pop: " + myStack.pop());
 
         System.out.println("\nIsEmpty: " + myStack.isEmpty());
+
+        System.out.println("======================");
+
+        Queue<Integer> myQueue = new LinkedList<>();
+        myQueue.add(1);
+        myQueue.add(2);
+        myQueue.add(3);
+
+        for (Integer i : myQueue) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\nRemove: " + myQueue.remove());
+        System.out.println("Remove: " + myQueue.remove());
+        System.out.println("Remove: " + myQueue.remove());
+
+        System.out.println("\nIsEmpty: " + myQueue.isEmpty());
     }
 }
